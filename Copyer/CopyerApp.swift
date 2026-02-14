d75d45bae52c07e9d2c37cc58014b68b2a28794a
Noam <noamramny@gmail.com>
2026-02-14 18:11:21 +0200

    Initial Commit

---
+++ Copyer/CopyerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CopyerApp.swift
+//  Copyer
+//
+//  Created by Noam Ram on 2/14/26.
+//
+
+import SwiftUI
+
+@main
+struct CopyerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
